ac63b0bea7c4be6559987e72512bfb3fb2574ba8
yellowAcute <846739308@qq.com>
2024-02-28 10:41:51 +0800

    ddd

--- test.cpp
+++ test.cpp
@@ -9,8 +9,6 @@ int main(int argc, char const *argv[])
     cout << "hello world" << endl;
     cout << "hello world" << endl;
     cout << "hello world" << endl;
-    cout << "hello world" << endl;
-    cout << "hello world" << endl;
 
     return 0;
 }
